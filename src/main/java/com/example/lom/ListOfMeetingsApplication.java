a4cf440ffa1bb976d0cf6618f808e7253252df54
iron-66 <ironman4000000@gmail.com>
2024-03-18 17:34:06 +0500

    Spring files uploading

---
+++ src/main/java/com/example/lom/ListOfMeetingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfMeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfMeetingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lom/ListOfMeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfMeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
